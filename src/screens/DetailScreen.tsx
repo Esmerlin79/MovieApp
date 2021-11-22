a1fef4379b9762bcede36479568428a85887c4a5
Esmerlin Elivo <eselivo@contractor.appsbees.com>
2021-11-22 10:01:15 -0400

    add Stack navigation

---
+++ src/screens/DetailScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const DetailScreen = () => {
+    return (
+        <View>
+            <Text>DetailScreen</Text>
+        </View>
+    )
+}
+
+export default DetailScreen
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+const HomeScreen = () => {
+
+    return (
+        <View>
+            <Text>homeScreen</Text>
+        </View>
+    )
+}
+
+export default HomeScreen
